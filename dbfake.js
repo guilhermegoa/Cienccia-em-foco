7e8da39b9a542832af391233823b92ffd99f1afb
[Guilherme Oliveira] <[guilhergoa@hotmail.com]>
2019-05-23 09:24:40 -0300

    java  e  json

---
+++ dbfake.js
@@ -0,0 +1,63 @@
+var db1 = {
+    noti: [
+        {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/1',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/2',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/3',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/4',
+            categoria: 'Ciências',
+
+        }
+    ]
+}
+
+var db2 = {
+    noti: [
+        {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/1',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/2',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/3',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/4',
+            categoria: 'Ciências',
+
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/5',
+            categoria: 'Ciências',
+        }, {
+            titulo: 'Lorem ipsum dolor',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+            imagem: 'http://lorempixel.com/300/200/technics/5',
+            categoria: 'Ciências',
+        }
+    ]
+}
